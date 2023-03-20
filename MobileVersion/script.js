8770c54250f332dd6f6bb89c77025c951d1ba4f9
Eric Collard <ruffyreborn@gmail.com>
2023-03-20 18:23:08 -0500

    Completed mobile version of the image slider. Streamlined HTML and CSS to better use code across projects and avoid interfering with common code.

--- MobileVersion/script.js
+++ MobileVersion/script.js
@@ -1,61 +1,19 @@
-// const sliderContainer = document.querySelector('.slider-container');
-// const slideRight = document.querySelector('.right-slide');
-// const slideLeft = document.querySelector('.left-slide');
-// const upButton = document.querySelector('.up-button');
-// const downButton = document.querySelector('.down-button');
-// const slidesLength = slideRight.querySelectorAll('div').length;
-// console.log(slidesLength);
-// let activeSlideIndex = 0;
-
-// slideLeft.style.top = `-${(slidesLength - 1)}`;
-
-// upButton.addEventListener('click', () => changeSlide('up'));
-// downButton.addEventListener('click', () => changeSlide('down'));
-
-// const changeSlide = (direction) => {
-// const sliderHeight = sliderContainer.clientHeight * .9;
-// console.log(sliderHeight);
-// if(direction === 'up') {
-//     activeSlideIndex ++;
-//     if (activeSlideIndex > slidesLength - 1) {
-//         activeSlideIndex = 0;
-//     }
-//     } else if (direction === 'down') {
-//         activeSlideIndex --;
-//     if (activeSlideIndex < 0) {
-//         activeSlideIndex = slidesLength - 1;
-//     }
-// }
-// slideRight.style.transform = `translateY(-${activeSlideIndex * sliderHeight}px)`
-// slideLeft.style.transform = `translateY(${activeSlideIndex * sliderHeight}px)`
-// }
-
-
-
-
-
-
-const sliderContainer = document.querySelector('.slider-container');
 const sliderContainer1 = document.querySelector('.slider-container1');
 const sliderContainer2 = document.querySelector('.slider-container2');
 const slideRight = document.querySelector('.right-slide');
 const slideLeft = document.querySelector('.left-slide');
-const upButton = document.querySelector('.up-button');
-const downButton = document.querySelector('.down-button');
+const upButton = document.querySelector('.slider-up-button');
+const downButton = document.querySelector('.slider-down-button');
 const slidesLength = slideRight.querySelectorAll('.slide').length;
-// const slidesLength = slideRight.document.querySelectorAll('div').length;
 console.log(slidesLength);
 let activeSlideIndex = 0;
-
-slideLeft.style.top = `${(slidesLength - 1)}`;
 
 upButton.addEventListener('click', () => changeSlide('up'));
 downButton.addEventListener('click', () => changeSlide('down'));
 
 const changeSlide = (direction) => {
-const sliderHeightT = sliderContainer1.clientHeight;
+const sliderHeightT = -sliderContainer1.clientHeight;
 const sliderHeightB = sliderContainer2.clientHeight;
-// const sliderHeight = sliderContainer.clientHeight;
 if(direction === 'up') {
     activeSlideIndex ++;
     if (activeSlideIndex > slidesLength - 1) {
@@ -68,14 +26,8 @@ if(direction === 'up') {
     }
 }
 slideRight.style.transform = `translateY(-${activeSlideIndex * sliderHeightB}px)`
-slideLeft.style.transform = `translateY(-${activeSlideIndex * sliderHeightT}px)`
-// slideRight.style.transform = `translateY(-${activeSlideIndex * sliderHeightB}px)`
-// slideLeft.style.transform = `translateY(${activeSlideIndex * sliderHeightT}px)`
+slideLeft.style.transform = `translateY(${activeSlideIndex * sliderHeightT}px)`
 }
-// slideRight.style.transform = `translateY(-${activeSlideIndex * sliderHeightB}px)`
-// slideLeft.style.transform = `translateY(${activeSlideIndex * sliderHeightT}px)`
-// }
-
 
 
 
